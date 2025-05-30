8b6aa7dfa8aab8c7d3a3911fec518950e30bae62
ujjwal405 <silwalujjwal3@gmail.com>
2025-05-30 17:09:13 +0545

    added correct version string

--- terraform/modules/permission/main.tf
+++ terraform/modules/permission/main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket_policy" "this" {
   bucket = var.bucket_id
   policy = jsonencode({
-    Version = "2025-05-17"
+    Version = "2025-10-17"
     Statement = [
       {
         Sid    = "AllowCloudFrontServicePrincipalReadOnly"
