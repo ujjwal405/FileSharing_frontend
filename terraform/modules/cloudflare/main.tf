6dc9f46ee508070d2f51ae63482a0e90785eb1ac
ujjwal405 <silwalujjwal3@gmail.com>
2025-05-30 16:00:44 +0545

    added ttl

--- terraform/modules/cloudflare/main.tf
+++ terraform/modules/cloudflare/main.tf
@@ -4,6 +4,7 @@ resource "cloudflare_dns_record" "this" {
   zone_id = var.cloudflare_zone_id
   name    = var.cloudflare_sub_domain_name
   content = var.cloudflare_record_content
+  ttl     = 3600
   type    = "CNAME"
   proxied = false
 }
