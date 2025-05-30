de97c0ecd67a5ce25b8806970f347580839d517e
ujjwal405 <silwalujjwal3@gmail.com>
2025-05-30 17:19:48 +0545

    resource arn corrected

--- terraform/modules/permission/main.tf
+++ terraform/modules/permission/main.tf
@@ -10,7 +10,7 @@ resource "aws_s3_bucket_policy" "this" {
           Service = "cloudfront.amazonaws.com"
         }
         Action   = "s3:GetObject"
-        Resource = "${var.bucket_arn}}/*"
+        Resource = "${var.bucket_arn}/*"
         Condition = {
           StringEquals = {
             "AWS:SourceArn" = "${var.cloudfront_distribution_arn}"
