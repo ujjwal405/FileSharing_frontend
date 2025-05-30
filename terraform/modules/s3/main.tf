04fdba27cb54a723044d40c6aa8304dd33d1354e
ujjwal405 <silwalujjwal3@gmail.com>
2025-05-30 17:39:02 +0545

    added content_type

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,3 +1,13 @@
+locals {
+  mime_types = {
+    html = "text/html"
+    css  = "text/css"
+    js   = "application/javascript"
+    png  = "image/png"
+    jpg  = "image/jpeg"
+    svg  = "image/svg+xml"
+  }
+}
 resource "aws_s3_bucket" "this" {
   bucket        = var.bucket_name
   force_destroy = var.force_destroy
@@ -50,11 +60,11 @@ resource "aws_s3_bucket_website_configuration" "this" {
 resource "aws_s3_object" "this" {
   for_each = fileset("${path.module}/../../../${var.frontend_directory}", "*.html") # Match only .html files
 
-  bucket = aws_s3_bucket.this.id
-  key    = each.value                                                        # Key in S3 (filename)
-  source = "${path.module}/../../../${var.frontend_directory}/${each.value}" # Local file path (using path.module)
+  bucket       = aws_s3_bucket.this.id
+  key          = each.value                                                        # Key in S3 (filename)
+  source       = "${path.module}/../../../${var.frontend_directory}/${each.value}" # Local file path (using path.module)
+  content_type = lookup(local.mime_types, split(".", each.value)[1], "binary/octet-stream")
+
 }
-
-
 
 
